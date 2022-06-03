1fb647e07f5fdeaf67bd566635faf62aa048f426
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-06-03 12:22:16 +0100

    cloudinary working

--- src/apis/products/index.js
+++ src/apis/products/index.js
@@ -1,83 +1,75 @@
-import express from 'express'
+import express from "express";
 import {
   getProducts,
   writeProducts,
   getReviews,
   writeReviews,
-  getProductsReadableStream
-} from '../../lib/fs-tools.js'
-import createError from 'http-errors'
-import ProductModel from './model.js'
-import q2m from 'query-to-mongo'
-import multer from 'multer'
-import { CloudinaryStorage } from 'multer-storage-cloudinary'
-import { v2 as cloudinary } from 'cloudinary'
-/* import { saveProductsPicture } from "../../lib/fs-tools.js"; */
+  getProductsReadableStream,
+} from "../../lib/fs-tools.js";
+import createError from "http-errors";
+import ProductModel from "./model.js";
+import q2m from "query-to-mongo";
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 
-const productsRouter = express.Router()
+const productsRouter = express.Router();
 
 const cloudinaryUploader = multer({
   storage: new CloudinaryStorage({
     cloudinary, // this searches in your process.env for something called CLOUDINARY_URL, which contains your API environment variable
     params: {
-      folder: 'm6d10'
-    }
-  })
-  /*   fileFilter: (req, file, multerNext) => {
-      if (file.mimetype !== "image/gif") {
-        multerNext(createError(400, "Only GIF allowed!"));
-      } else {
-        multerNext(null, true);
-      }
+      folder: "m6d10",
     },
-    limits: { fileSize: 1 * 1024 * 1024 }, */
-}).single('picture')
+  }),
+  fileFilter: (req, file, multerNext) => {
+    if (file.mimetype !== "image/jpeg" && file.mimetype !== "image/png") {
+      multerNext(createError(400, "Only PNG or JPEG allowed!"));
+    } else {
+      multerNext(null, true);
+    }
+  },
+  limits: { fileSize: 1 * 1024 * 1024 },
+}).single("picture");
 
 productsRouter.post(
-  '/:productId/upload',
+  "/:productId/upload",
   cloudinaryUploader,
   async (req, res, next) => {
     try {
-      console.log('FILE: ', req.file)
-      const products = await ProductModel.find()
-      const index = products.findIndex(
-        (product) => product._id === req.params.productId
-      )
-      console.log('INDEX OF  :', index)
-      const oldProduct = products[index]
-      const newProduct = {
-        ...oldProduct,
-        imageUrl: req.file.path
-      }
-      console.log(req.file)
-      products[index] = newProduct
-      writeProducts(products)
+      const products = await ProductModel.findByIdAndUpdate(
+        req.params.productId,
+        { imageUrl: req.file.path },
+        { new: true, runValidators: true }
+      );
 
-      res.send()
+      console.log("FILE: ", req.file);
+
+      res.send();
     } catch (error) {
-      console.log(error)
-      res.status(500).send({ error: error.message })
+      console.log(error);
+      res.status(500).send({ error: error.message });
       /* next(error); */
     }
   }
-)
+);
 
-productsRouter.post('/', async (req, res, next) => {
+productsRouter.post("/", async (req, res, next) => {
   try {
-    const newProduct = new ProductModel(req.body)
-    const { _id } = await newProduct.save()
-    res.status(201).send({ _id })
+    const newProduct = new ProductModel(req.body);
+    const { _id } = await newProduct.save();
+    res.status(201).send({ _id });
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-productsRouter.get('/', async (req, res, next) => {
+productsRouter.get("/", async (req, res, next) => {
   try {
-    console.log('QUERY: ', req.query)
-    console.log('MONGO-QUERY: ', q2m(req.query))
-    const mongoQuery = q2m(req.query)
-    const total = await ProductModel.countDocuments(mongoQuery.criteria)
+    console.log("QUERY: ", req.query);
+    console.log("MONGO-QUERY: ", q2m(req.query));
+    const mongoQuery = q2m(req.query);
+    const total = await ProductModel.countDocuments(mongoQuery.criteria);
     const Products = await ProductModel.find(
       mongoQuery.criteria,
       mongoQuery.options.fields
@@ -85,72 +77,72 @@ productsRouter.get('/', async (req, res, next) => {
       .skip(mongoQuery.options.skip)
       .limit(mongoQuery.options.limit)
       .sort(mongoQuery.options.sort)
-      .populate({ path: 'reviews', select: 'comment text' })
+      .populate({ path: "reviews", select: "comment text" });
     res.send({
-      links: mongoQuery.links('http://localhost:3001/products', total),
+      links: mongoQuery.links("http://localhost:3001/products", total),
       total,
       totalPages: Math.ceil(total / mongoQuery.options.limit),
-      Products
-    })
+      Products,
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-productsRouter.get('/:productId', async (req, res, next) => {
+productsRouter.get("/:productId", async (req, res, next) => {
   try {
     const product = await ProductModel.findById(
       req.params.productId
-    ) /* .populate({
+    ); /* .populate({
       path: "authors",
       select: "firstName lastName",
     }) */
     if (product) {
-      res.send(product)
+      res.send(product);
     } else {
       next(
         createError(404, `Product with id ${req.params.productId} not found!`)
-      )
+      );
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-productsRouter.put('/:productId', async (req, res, next) => {
+productsRouter.put("/:productId", async (req, res, next) => {
   try {
     const updatedProduct = await ProductModel.findByIdAndUpdate(
       req.params.productId,
       req.body,
       { new: true, runValidators: true }
-    )
+    );
     if (updatedProduct) {
-      res.send(updatedProduct)
+      res.send(updatedProduct);
     } else {
       next(
         createError(404, `Product with id ${req.params.productId} not found!`)
-      )
+      );
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-productsRouter.delete('/:productId', async (req, res, next) => {
+productsRouter.delete("/:productId", async (req, res, next) => {
   try {
     const deletedProduct = await ProductModel.findByIdAndDelete(
       req.params.productId
-    )
+    );
     if (deletedProduct) {
-      res.status(204).send()
+      res.status(204).send();
     } else {
       next(
         createError(404, `Product with id ${req.params.productId} not found!`)
-      )
+      );
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-export default productsRouter
+export default productsRouter;
